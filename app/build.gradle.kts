702b996bd7eef54f83ff95240ad92b3374616d16
Mokhtari Hadj Mohamed <7adj.mo7amed@gmail.com>
2025-07-19 22:56:20 +0100

    add test for order test calculation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,4 +59,5 @@ dependencies {
 
     // TODO NEW LIB
     implementation(libs.androidx.navigation.compose)
+    testImplementation(kotlin("test"))
 }
---
+++ app/src/test/java/com/mokhtarihadjmohamed/thetag/OrderUnitTest.kt
@@ -0,0 +1,40 @@
+package com.mokhtarihadjmohamed.thetag
+
+import com.mokhtarihadjmohamed.thetag.model.Order
+import com.mokhtarihadjmohamed.thetag.model.Product
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import kotlin.test.assertNotEquals
+
+class OrderUnitTest {
+    val order = Order(
+        id = "458",
+        products = listOf(
+            Product(
+                name = "Burger  - SM Lucena",
+                description = "Grillé à la flamme pour un goût inimitable",
+                price = 18.0,
+                imageURl = "",
+                amount = 1
+            ),
+            Product(
+                name = "Burger  - SM Lucena",
+                description = "Grillé à la flamme pour un goût inimitable",
+                price = 18.0,
+                imageURl = "",
+                amount = 2
+            )
+        ),
+        date = "8 Avril 2025"
+    )
+
+    @Test
+    fun total_is_correct_false() {
+        assertNotEquals(44.0, order.getTotal(), 0.001)
+    }
+
+    @Test
+    fun total_is_correct_true() {
+        assertEquals(54.0, order.getTotal(), 0.001)
+    }
+}
